8be9dd0626deddf42407fd11045cfb1cbab9156d
luismiguelcasadodiaz <luismiguelcasadodiaz@gmail.com>
2021-12-27 10:19:09 +0000

    Control si hay fileneme o no en la generación del mensaje de mail

--- emails.py
+++ emails.py
@@ -2,16 +2,17 @@ import smtplib
 from email.message import EmailMessage
 import mimetypes
 import os
-def generate_mail(sender, receiver, subject, body, path_to_attach ):
+def generate_mail(sender, receiver, subject, body, path_to_attach,filename=None ):
     m=EmailMessage()
     m["From"]=sender
     m["To"]=receiver
     m["Subject"]=subject 
     m.set_content(body)
-    mime_type, _ = mimetypes.guess_type(path_to_attach)
-    m_type, m_subtype = mime_type.split("/",1)
-    with open(path_to_attach, 'rb') as attach:
-        m.add_attachment(attach.read(),
+    if filename not None:
+        mime_type, _ = mimetypes.guess_type(path_to_attach)
+        m_type, m_subtype = mime_type.split("/",1)
+        with open(path_to_attach, 'rb') as attach:
+            m.add_attachment(attach.read(),
                 maintype = m_type, 
                 subtype = m_subtype, 
                 filename = path_to_attach)
@@ -24,11 +25,3 @@ def send_mail(msg):
     server.quit()
     pass
 
-if __name__ ==  "__main__":
-    sender="automation@example.com"
-    receiver="username@example.com"
-    subject = "Upload Completed - Online Fruit Store"
-    body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
-    path_to_attach="BarChart11000.pdf"
-    msg=generate_mail(sender, receiver, subject, body, path_to_attach )
-    send_mail(msg)
