11a9c0023ef77b65d7ab4a5acf9ac9911f46a540
luismiguelcasadodiaz <luismiguelcasadodiaz@gmail.com>
2021-12-27 11:45:46 +0000

    SI myfilename no es un parametreo de la función no puedo usarlos

--- emails.py
+++ emails.py
@@ -3,16 +3,16 @@ from email.message import EmailMessage
 import mimetypes
 import os
 def generate_mail(sender, receiver, subject, body, path_to_attach=None):
-    myfilename = os.path.basename(path_to_attach)
     m=EmailMessage()
     m["From"]=sender
     m["To"]=receiver
     m["Subject"]=subject 
     m.set_content(body)
-    if myfilename != None:
-        mime_type, _ = mimetypes.guess_type(path_to_attach)
-        m_type, m_subtype = mime_type.split("/",1)
-        with open(path_to_attach, 'rb') as attach:
+    if path_to_attach != None:
+       myfilename = os.path.basename(path_to_attach)
+       mime_type, _ = mimetypes.guess_type(path_to_attach)
+       m_type, m_subtype = mime_type.split("/",1)
+       with open(path_to_attach, 'rb') as attach:
             m.add_attachment(attach.read(),
                 maintype = m_type, 
                 subtype = m_subtype, 
