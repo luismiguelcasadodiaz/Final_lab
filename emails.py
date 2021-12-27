59e04eeed4864e6c6c7c71ecebd2ea6bb0264e0e
luismiguelcasadodiaz <luismiguelcasadodiaz@gmail.com>
2021-12-27 11:24:04 +0100

    Elimino error comparando filename con None

--- emails.py
+++ emails.py
@@ -8,7 +8,7 @@ def generate_mail(sender, receiver, subject, body, path_to_attach,filename=None 
     m["To"]=receiver
     m["Subject"]=subject 
     m.set_content(body)
-    if filename not None:
+    if filename != None:
         mime_type, _ = mimetypes.guess_type(path_to_attach)
         m_type, m_subtype = mime_type.split("/",1)
         with open(path_to_attach, 'rb') as attach:
